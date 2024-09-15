cb6b977c301f26ef60eb41d51ee3efd07bb60b00
dhiraj <dhirajaarya43@gmail.com>
2024-09-15 15:33:05 +0545

    html css project

---
+++ responsive.js
@@ -0,0 +1,12 @@
+// burger = document.querySelector('.burger')
+// navbar = document.querySelector('.navbar')
+// navList = document.querySelector('.lists')
+// rightnav = document.querySelector('.nav-search')
+
+
+
+// burger.addEventListener('click', ()=>{
+//     rightnav.classList.toggle('v-class-resp');
+//     navList.classList.toggle('v-class-resp');
+//     navbar.classList.toggle('h-nav-resp');
+// })
